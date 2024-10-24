5fb0bd16e218297a601e3698dfe9ec23b7f89dea
Donovan <donovan.roberts27@gmail.com>
2024-10-24 18:53:11 -0400

    Added FizzBuzz program code.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,11 @@
 # add your code here
-
+numbers = range(1, 101)
+for number in numbers:
+    if number % 3 == 0 and number % 5 == 0:
+        print('FizzBuzz')
+    elif number % 3 == 0:
+        print('Fizz')
+    elif number % 5 == 0:
+        print('Buzz')
+    else:
+        print(number)
